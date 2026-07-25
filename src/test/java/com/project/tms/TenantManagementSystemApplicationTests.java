c8752350ffc8d5d71b2488c71f19cd8e6da3c166
RishabhKumar <rishabhkumar.rk755@gmail.com>
2026-07-25 22:20:29 +0530

    Initial project setup - Tenant Management System

---
+++ src/test/java/com/project/tms/TenantManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.tms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TenantManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
